eab0f17d18949bebbb9b2d13089ea5788d6c8ea0
hanella91 <hanella0911@gmail.com>
2021-11-01 14:21:54 +0900

    navabar scroll to view

--- main.js
+++ main.js
@@ -17,10 +17,11 @@ const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
   const target = event.target;
   const link = target.dataset.link;
+
   if( link == null ){
     return;
   }
-  console.log(event.target.dataset.link);
+
   const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView();
+  scrollTo.scrollIntoView({ behavior: 'smooth'});
 })
